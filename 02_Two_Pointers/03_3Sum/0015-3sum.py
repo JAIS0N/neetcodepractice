75f3a291f29526765c85d7f09a47da56d3271de6
Jaison Menezes <46769916+JAIS0N@users.noreply.github.com>
2025-10-06 16:35:38 -0700

    Added hash set solution as well in comments

--- 02_Two_Pointers/03_3Sum/0015-3sum.py
+++ 02_Two_Pointers/03_3Sum/0015-3sum.py
@@ -18,3 +18,31 @@ def threeSum(self, nums: List[int]) -> List[List[int]]:
                     while nums[l]==nums[l-1] and l<r:
                         l=l+1
         return res
+
+
+
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()
+#         n = len(nums)
+#         res = set()                # store unique triplets as tuples
+
+#         for i in range(n):
+#             a = nums[i]
+#             if i > 0 and a == nums[i - 1]:   # skip duplicate 'a'
+#                 continue
+
+#             seen = set()           # values seen while scanning for this 'a'
+#             target = -a
+#             for j in range(i + 1, n):
+#                 need = target - nums[j]
+#                 if need in seen:
+#                     # (a <= need <= nums[j]) because array is sorted and 'need' came from earlier j
+#                     res.add((a, need, nums[j]))
+#                 seen.add(nums[j])
+
+#         return [list(t) for t in res]
+
+
+        
