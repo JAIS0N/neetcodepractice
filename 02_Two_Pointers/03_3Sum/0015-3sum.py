c141450a00730b782aead38ba7caa4b0de118ec4
Jaison Menezes <menezesjaison@gmail.com>
2026-02-03 16:25:16 -0800

    Refactor threeSum function for clarity and efficiency

--- 02_Two_Pointers/03_3Sum/0015-3sum.py
+++ 02_Two_Pointers/03_3Sum/0015-3sum.py
@@ -1,24 +1,37 @@
-def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res=[]
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
         nums.sort()
-        for i,a in enumerate(nums):
-            if i>0 and a==nums[i-1]:
-                    continue
-            l=i+1
-            r=len(nums)-1
-            while l<r:
-                threesum=a+nums[l]+nums[r]
-                if threesum>0:
-                    r=r-1
-                elif threesum<0:
-                    l=l+1
-                else:
-                    res.append([a,nums[l],nums[r]])
-                    l=l+1
-                    while nums[l]==nums[l-1] and l<r:
-                        l=l+1
-        return res
+        n = len(nums)
 
+        for i in range(n):
+            # Skip duplicate fixed elements
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+
+                if current_sum < 0:
+                    left += 1
+                elif current_sum > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate left and right values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return result
 
 
 
