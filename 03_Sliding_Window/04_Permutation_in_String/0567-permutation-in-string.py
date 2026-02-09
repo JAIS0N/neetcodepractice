cd568cdebcbd54fe55029d4eca5c60018a0d6578
Jaison Menezes <menezesjaison@gmail.com>
2026-02-08 18:34:11 -0800

    Optimize permutation check with frequency arrays
    Refactor permutation check using frequency arrays for efficiency.

--- 03_Sliding_Window/04_Permutation_in_String/0567-permutation-in-string.py
+++ 03_Sliding_Window/04_Permutation_in_String/0567-permutation-in-string.py
@@ -1,40 +1,24 @@
-"""
-Problem: LeetCode 567 - Permutation in String
-
-Key Idea:
-To check whether a string 's2' contains a permutation of another string 's1', we can use a sliding window approach. First, we create a frequency dictionary for characters in 's1'. Then, we initialize two pointers, 'left' and 'right', to represent the current window in 's2'. As we move the 'right' pointer to the right, we add the character to a temporary frequency dictionary and check if it becomes equal to the frequency dictionary of 's1'. If it does, it means we found a permutation of 's1' in 's2', and we return True. If the window size exceeds the length of 's1', we remove the character at the 'left' pointer from the temporary dictionary and move the 'left' pointer to the right to shrink the window. We continue this process until we find a permutation or reach the end of 's2'.
-
-Time Complexity:
-The time complexity of this solution is O(n), where n is the length of 's2'. The sliding window approach iterates through 's2' once, and at each step, we perform constant-time operations to update the window and the frequency dictionaries.
-
-Space Complexity:
-The space complexity is O(1) since the frequency dictionaries have a constant number of elements (the number of unique characters in 's1' or the alphabet size) regardless of the size of the input strings 's1' and 's2'.
-"""
-
-
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
 
-        char_freq_s1 = {}
+        freq_s1 = [0] * 26
+        freq_window = [0] * 26
+
         for char in s1:
-            char_freq_s1[char] = char_freq_s1.get(char, 0) + 1
+            freq_s1[ord(char) - ord('a')] += 1
 
-        left, right = 0, 0
-        char_freq_temp = {}
+        left = 0
 
-        while right < len(s2):
-            char_freq_temp[s2[right]] = char_freq_temp.get(s2[right], 0) + 1
+        for right in range(len(s2)):
+            freq_window[ord(s2[right]) - ord('a')] += 1
 
-            if right - left + 1 == len(s1):
-                if char_freq_temp == char_freq_s1:
-                    return True
-                char_freq_temp[s2[left]] -= 1
-                if char_freq_temp[s2[left]] == 0:
-                    del char_freq_temp[s2[left]]
+            if right - left + 1 > len(s1):
+                freq_window[ord(s2[left]) - ord('a')] -= 1
                 left += 1
 
-            right += 1
+            if freq_window == freq_s1:
+                return True
 
         return False
