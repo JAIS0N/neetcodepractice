dbdd38b774a8acc5898186e80fab3e3fac9f01ad
Jaison Menezes <menezesjaison@gmail.com>
2026-02-16 14:05:00 -0800

    Refactor evalRPN function for clarity and efficiency

--- 04_Stack/03_Evaluate_Reverse_Polish_Notation/0150-evaluate-reverse-polish-notation.py
+++ 04_Stack/03_Evaluate_Reverse_Polish_Notation/0150-evaluate-reverse-polish-notation.py
@@ -1,34 +1,22 @@
-"""
-Problem: LeetCode 150 - Evaluate Reverse Polish Notation
-
-Key Idea:
-To evaluate a given reverse Polish notation expression, we can use a stack data structure. We iterate through the tokens in the expression, and for each token, if it is a number, we push it onto the stack. If it is an operator ('+', '-', '*', '/'), we pop the top two elements from the stack, apply the operator to them, and push the result back onto the stack. At the end, the top element of the stack will be the final result of the expression.
-
-Time Complexity:
-The time complexity of this solution is O(n), where n is the number of tokens in the expression. We iterate through the tokens once, and each operation (pushing, popping, and applying operators) takes constant time.
-
-Space Complexity:
-The space complexity is O(n), where n is the number of tokens in the expression. In the worst case, the stack could store all tokens from the input expression.
-"""
-
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-
-        for token in tokens:
-            if token.isdigit() or (token[0] == "-" and token[1:].isdigit()):
-                stack.append(int(token))
+        stack=[]
+        for t in tokens:
+            if t not in "+-*/":
+                stack.append(int(t))
             else:
-                num2 = stack.pop()
-                num1 = stack.pop()
-                if token == "+":
-                    stack.append(num1 + num2)
-                elif token == "-":
-                    stack.append(num1 - num2)
-                elif token == "*":
-                    stack.append(num1 * num2)
-                elif token == "/":
-                    stack.append(int(num1 / num2))
-
+                a=stack.pop()
+                b=stack.pop()
+                if t=="+":
+                    stack.append(a+b)    
+                elif t=="-":
+                    stack.append(b-a)
+                elif t=="*":
+                    stack.append(a*b)
+                elif t=="/":
+                    stack.append(int(b/a))
         return stack[0]
+
+
+        
+        
